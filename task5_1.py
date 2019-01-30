e94713a46f3dcc2326b8932a4fe952c6dc3e2ac9
praneethm45 <pr642175@dal.ca>
2019-01-30 11:16:12 -0800

    distance

---
+++ task5_1.py
@@ -0,0 +1,29 @@
+def class Point:
+    def __init__(self, initX, initY):
+
+        self.x = initX
+        self.y = initY
+
+    def getX(self):
+        return self.x
+
+    def getY(self):
+        return self.y
+
+    def distanceFromOrigin(self):
+        return ((self.x ** 2) + (self.y ** 2)) ** 0.5
+
+
+p = Point(7,6)
+print(p.distanceFromOrigin())getX(self):
+        return self.x
+
+    def getY(self):
+        return self.y
+
+    def distanceFromOrigin(self):
+        return ((self.x ** 2) + (self.y ** 2)) ** 0.5
+
+
+p = Point(7,6)
+print(p.distanceFromOrigin())
